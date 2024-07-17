c68897806efa50b325a532e58030495501ad1e61
asuma3399 <fujimoto3399@gmail.com>
2024-07-17 20:14:03 +0900

    レビュー指摘点の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,8 +11,7 @@ class Item < ApplicationRecord
   validates :product_name, presence: true
   validates :product_description, presence: true
   validates :category_id, numericality: { other_than: 1 }
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'is not a number', allow_blank: true },
-                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, allow_blank: true }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, allow_blank: true }
   validates :product_condition_id, numericality: { other_than: 1 }
   validates :burden_of_shipping_charges_id, numericality: { other_than: 1 }
   validates :delivery_region_id, numericality: { other_than: 1 }
